9bf4c9d03001752951e2fd5030b8dc3a39e430bb
dawnniie <lachlan.walls1@gmail.com>
2025-08-04 15:30:47 +1000

    chore: update lockfile and build output dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build'
+    outDir: 'dist',
   },
   plugins: [react()],
 })
